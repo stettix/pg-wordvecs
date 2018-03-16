a4de5bbd7e68c158a72b7f701241a9643ab4e9f8
stettix <jan.stette@gmail.com>
2018-03-16 12:17:08 +0000

    Add code for loading word vectors from file
    Also, create table with index in setup of repository

--- src/main/scala/net/janvsmachine/pgwordvecs/LoadVectors.scala
+++ src/main/scala/net/janvsmachine/pgwordvecs/LoadVectors.scala
@@ -1,7 +1,12 @@
 package net.janvsmachine.pgwordvecs
 
+import java.nio.file.Paths
+
 import doobie._
-import cats.effect._
+import doobie.implicits._
+import cats.implicits._
+import cats.effect.IO
+
 
 /**
   * Command line utility for loading GloVe word vectors into PostgreSQL.
@@ -23,15 +28,35 @@ object LoadVectors extends App {
 
   val repo = new WordVectorsRepo("glove_6b_50d")
   repo.init()
-  repo.insertExamples()
+  insertExamples()
+  repo.loadFromFile(Paths.get("/Users/jan/data/wordvecs/glove/glove.6B.50d.txt"))
+
+  def insertExamples(): Unit = {
+    val i1: ConnectionIO[Int] = repo.insert(WordVector("is", Vector(0.6185, 0.64254, -0.46552, 0.3757, 0.74838, 0.53739, 0.0022239, -0.60577, 0.26408, 0.11703, 0.43722, 0.20092, -0.057859, -0.34589, 0.21664, 0.58573, 0.53919, 0.6949, -0.15618, 0.05583, -0.60515, -0.28997, -0.025594, 0.55593, 0.25356, -1.9612, -0.51381, 0.69096, 0.066246, -0.054224, 3.7871, -0.77403, -0.12689, -0.51465, 0.066705, -0.32933, 0.13483, 0.19049, 0.13812, -0.21503, -0.016573, 0.312, -0.33189, -0.026001, -0.38203, 0.19403, -0.12466, -0.27557, 0.30899, 0.48497)))
+    val i2: ConnectionIO[Int] = repo.insert(WordVector("was", Vector(0.086888, -0.19416, -0.24267, -0.33391, 0.56731, 0.39783, -0.97809, 0.03159, -0.61469, -0.31406, 0.56145, 0.12886, -0.84193, -0.46992, 0.47097, 0.023012, -0.59609, 0.22291, -1.1614, 0.3865, 0.067412, 0.44883, 0.17394, -0.53574, 0.17909, -2.1647, -0.12827, 0.29036, -0.15061, 0.35242, 3.124, -0.90085, -0.02567, -0.41709, 0.40565, -0.22703, 0.76829, 0.60982, 0.070068, -0.13271, -0.1201, 0.096132, -0.43998, -0.48531, -0.5188, -0.3077, -0.75028, -0.77, 0.3945, -0.16937)))
+    val res = (i1, i2).mapN(_ + _).transact(xa).unsafeRunSync
+    println(s"Insert examples result: $res")
+  }
+
 
   // Run some example queries.
 
-  val wordVec: Option[WordVector] = repo.vectorForWord("the")
-  println(s"Vector for 'the': $wordVec")
+  {
+    val wordVec: Option[WordVector] = repo.vectorForWord("the")
+    println(s"Vector for 'the': $wordVec")
 
-  val mostSimilar: Option[Vector[WordVector]] = wordVec.map(wv => repo.mostSimilarVectors(wv.vector))
-  println(s"Most similar vectors for 'the':")
-  mostSimilar.foreach(_.foreach(println))
+    val mostSimilar: Option[Vector[WordVector]] = wordVec.map(wv => repo.mostSimilarVectors(wv.vector))
+    println(s"Most similar vectors for 'the':")
+    mostSimilar.foreach(_.foreach(println))
+  }
+
+  {
+    val wordVec: Option[WordVector] = repo.vectorForWord("fish")
+    println(s"Vector for 'fish': $wordVec")
+
+    val mostSimilar: Option[Vector[WordVector]] = wordVec.map(wv => repo.mostSimilarVectors(wv.vector))
+    println(s"Most similar vectors for 'fish':")
+    mostSimilar.foreach(_.foreach(println))
+  }
 
 }
--- src/main/scala/net/janvsmachine/pgwordvecs/WordVectorsRepo.scala
+++ src/main/scala/net/janvsmachine/pgwordvecs/WordVectorsRepo.scala
@@ -17,38 +17,56 @@ import scala.io.{Codec, Source}
 
 class WordVectorsRepo(tableName: String)(implicit xa: Transactor.Aux[IO, Unit]) {
 
+  import WordVectorsRepo._
+
+  private val table = Fragment.const(tableName)
+
   def init(): Unit = {
-    val create =
-      sql"""
-         create table if not exists glove_6b_50d (
+    val createTable =
+      (fr"create table if not exists " ++ Fragment.const(tableName) ++
+        fr""" (
           word text primary key,
           vector cube not null
-         )  """.update.run
+         )""").update.run
+    val createIndex =
+      (fr"create index if not exists " ++ Fragment.const(tableName + "_idx") ++
+        fr" on " ++ Fragment.const(tableName) ++
+        fr" using gist (vector)").update.run
 
-    val r: Int = create.transact(xa).unsafeRunSync
-    println(s"Create result: $r")
+    val ops = (createTable, createIndex).mapN(_ + _)
+
+    val r: Int = ops.transact(xa).unsafeRunSync
+    println(s"Create table and index result: $r")
   }
 
   def loadFromFile(path: Path): Unit = {
-    val lines = Source.fromFile(path.toFile)(Codec.UTF8).getLines()
-    val parsed = lines.map(parseGloveLine)
-    ???
-  }
+    println(s"Reading from file $path")
 
-  def insertExamples(): Unit = {
-    val i1 = insert(WordVector("is", Vector(0.6185, 0.64254, -0.46552, 0.3757, 0.74838, 0.53739, 0.0022239, -0.60577, 0.26408, 0.11703, 0.43722, 0.20092, -0.057859, -0.34589, 0.21664, 0.58573, 0.53919, 0.6949, -0.15618, 0.05583, -0.60515, -0.28997, -0.025594, 0.55593, 0.25356, -1.9612, -0.51381, 0.69096, 0.066246, -0.054224, 3.7871, -0.77403, -0.12689, -0.51465, 0.066705, -0.32933, 0.13483, 0.19049, 0.13812, -0.21503, -0.016573, 0.312, -0.33189, -0.026001, -0.38203, 0.19403, -0.12466, -0.27557, 0.30899, 0.48497)))
-    val i2 = insert(WordVector("was", Vector(0.086888, -0.19416, -0.24267, -0.33391, 0.56731, 0.39783, -0.97809, 0.03159, -0.61469, -0.31406, 0.56145, 0.12886, -0.84193, -0.46992, 0.47097, 0.023012, -0.59609, 0.22291, -1.1614, 0.3865, 0.067412, 0.44883, 0.17394, -0.53574, 0.17909, -2.1647, -0.12827, 0.29036, -0.15061, 0.35242, 3.124, -0.90085, -0.02567, -0.41709, 0.40565, -0.22703, 0.76829, 0.60982, 0.070068, -0.13271, -0.1201, 0.096132, -0.43998, -0.48531, -0.5188, -0.3077, -0.75028, -0.77, 0.3945, -0.16937)))
-    val res = (i1, i2).mapN(_ + _).transact(xa).unsafeRunSync
-    println(s"Insert examples result: $res")
+    // TODO: Stream this instead, and update in batches? Then again, this brute force implemenetations works fine...
+    val lines = Source.fromFile(path.toFile)(Codec.UTF8).getLines()
+    val parsed: List[WordVector] = lines.map(parseGloveLine).toList
+    println(s"Parsed ${parsed.size} word vectors")
+
+    val ops: List[Update0] = parsed.map(insertOp)
+    val combined: ConnectionIO[List[Int]] = ops.traverse(_.run)
+    val res: Seq[Int] = combined.transact(xa).unsafeRunSync
+
+    println("Results: " + res.distinct)
   }
 
   def insert(wv: WordVector): ConnectionIO[Int] = {
-    // TODO: table name
     val cube = PgCube(wv.vector)
-    sql"insert into glove_6b_50d (word, vector) values (${wv.word}, $cube)".update.run
+    val query = fr"insert into " ++ Fragment.const(tableName) ++ fr" (word, vector) values (${wv.word}, $cube) on conflict do nothing"
+    query.update.run
   }
 
-  private[pgwordvecs] def parseGloveLine(line: String): (String, Vector[Double]) = ??? // TODO!
+  private def insertOp(wv: WordVector): Update0 = {
+    val cube = PgCube(wv.vector)
+    val query = fr"insert into " ++ Fragment.const(tableName) ++
+      fr""" (word, vector) values (${wv.word}, $cube)
+            on conflict do nothing""".stripMargin
+    query.update
+  }
 
   // TODO: make async
   def vectorForWord(word: String): Option[WordVector] = {
@@ -69,5 +87,16 @@ class WordVectorsRepo(tableName: String)(implicit xa: Transactor.Aux[IO, Unit]) 
   }
 
   def mostSimilarWords(word: String): Vector[String] = ???
+
+}
+
+object WordVectorsRepo {
+
+  private[pgwordvecs] def parseGloveLine(line: String): WordVector = {
+    val fields = line.split("\\s").toVector
+    val (word, values) = (fields.head, fields.drop(1))
+    val vector = values.map(_.toDouble)
+    WordVector(word, vector)
+  }
 
 }
